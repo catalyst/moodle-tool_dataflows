54e368e0b8951ef9cb678bb795c4332820107c29
Kevin Pham <keevan.pham@gmail.com>
2022-05-06 16:13:14 +1000

    add debugging step which sends the input to debugging (as json) & tests

---
+++ tests/tool_dataflows_test.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace tool_dataflows;
+
+/**
+ * Units tests for the manager class.
+ *
+ * @package    tool_dataflows
+ * @author     Kevin Pham <kevinpham@catalyst-au.net>
+ * @copyright  Catalyst IT, 2022
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tool_dataflows_test extends \advanced_testcase {
+
+    /**
+     * Set up before each test
+     */
+    protected function setUp(): void {
+        parent::setUp();
+        $this->resetAfterTest();
+    }
+
+    /**
+     * @covers \tool_dataflows\step\debugging
+     */
+    public function test_debugging_step(): void {
+        $step = new step\debugging();
+        $input = [1, 2, 3, 4, 5];
+        $output = $step->execute($input);
+
+        // No changes are expected.
+        $this->assertEquals($input, $output);
+
+        // Debugging was called with the expected format.
+        $this->assertDebuggingCalled(json_encode($input));
+    }
+}
