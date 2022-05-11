6df23161fb46f8b12c2bc8f69f2b29f276f3307e
Kevin Pham <keevan.pham@gmail.com>
2022-05-11 17:15:04 +1000

    chore: version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022050402;
-$plugin->release = 2022050402;
+$plugin->version = 2022051100;
+$plugin->release = 2022051100;
 
 $plugin->requires = 2017051500;    // Our lowest supported Moodle (3.3.0).
 
