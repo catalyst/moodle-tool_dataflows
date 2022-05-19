b017863632695ce84b2e6112b01dc28acec4f4c5
Kevin Pham <keevan.pham@gmail.com>
2022-05-19 16:16:07 +1000

    feat: add yaml parsing, import support for dataflows & supporting tests

--- classes/step.php
+++ classes/step.php
@@ -18,6 +18,7 @@ namespace tool_dataflows;
 
 use core\persistent;
 use moodle_exception;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Dataflow Step persistent class
@@ -95,7 +96,7 @@ class step extends persistent {
     /**
      * Persists the dependencies (dependson) for this step into the database.
      */
-    private function update_depends_on() {
+    public function update_depends_on() {
         global $DB;
 
         $dependencies = $this->dependson;
@@ -106,7 +107,20 @@ class step extends persistent {
         // Update records in database.
         $dependencymap = [];
         foreach ($dependencies as $dependency) {
-            $dependencymap[] = ['stepid' => $this->id, 'dependson' => $dependency->id ?? $dependency];
+            // If the dependency is a string, then it is most likely referencing
+            // the internalid. In this case, it should query the DB and populate
+            // the expected id numeric value.
+            $dependson = $dependency->id ?? $dependency;
+            if (gettype($dependson) === 'string') {
+                $step = $DB->get_record(
+                    'tool_dataflows_steps',
+                    ['internalid' => $dependency, 'dataflowid' => $this->dataflowid],
+                    'id'
+                );
+                $dependson = $step->id;
+                // TODO: Throw an exception if dependson is null/missing, as it should exist by this stage.
+            }
+            $dependencymap[] = ['stepid' => $this->id, 'dependson' => $dependson];
         }
         $DB->delete_records('tool_dataflows_step_depends', ['stepid' => $this->id]);
         $DB->insert_records('tool_dataflows_step_depends', $dependencymap);
@@ -144,5 +158,33 @@ class step extends persistent {
         $this->update_depends_on();
 
         return $this;
+    }
+
+    /**
+     * Handling for importing an individual step based on the step relevant to the yml file.
+     *
+     * See dataflow->import for how this all strings together.
+     *
+     * @param      array $yaml full dataflow configuration as a php array
+     */
+    public function import($stepdata) {
+        // Set the name of this step, the key will be used if a name is not provided.
+        $this->name = $stepdata['name'] ?? $stepdata['id'];
+        // Sets the type of this step, which should be a FQCN.
+        $this->type = $stepdata['type'];
+        // Sets the description for this step.
+        $this->description = $stepdata['description'] ?? '';
+        // Set the internalid of this step, the key will be used if the id is not provided.
+        // TODO: See if there's a good reason to have an id field separate to simply using the key.
+        $this->internalid = $stepdata['id'];
+
+        // Set the config as a valid YAML string.
+        $this->config = Yaml::dump($stepdata['config'] ?? '');
+
+        // Set up the dependencies, connected to each other via their internal step ids.
+        if (!empty($stepdata['depends_on'])) {
+            $dependson = (array) $stepdata['depends_on'];
+            $this->depends_on($dependson);
+        }
     }
 }
--- tests/tool_dataflows_test.php
+++ tests/tool_dataflows_test.php
@@ -16,6 +16,8 @@
 
 namespace tool_dataflows;
 
+use Symfony\Component\Yaml\Yaml;
+
 defined('MOODLE_INTERNAL') || die();
 
 // Include lib.php functions that aren't included automatically for Moodle 37- and below.
@@ -215,6 +217,41 @@ class tool_dataflows_test extends \advanced_testcase {
         ];
         $isdag = \tool_dataflows\graph::is_dag($edges);
         $this->assertTrue($isdag);
+    }
+
+    /**
+     * Test the parsing and importing of a sample yaml file which could be used in an import.
+     *
+     * This test may likely expand to cover any specific cases used for dataflows.
+     *
+     * @covers \tool_dataflows\dataflow::import
+     */
+    public function test_dataflows_import() {
+        $content = file_get_contents(dirname(__FILE__) . '/fixtures/sample.yml');
+        $yaml = \Symfony\Component\Yaml\Yaml::parse($content);
+        $dataflow = new dataflow();
+        $dataflow->import($yaml);
+
+        // Test the dataflow name.
+        $this->assertEquals('Example Dataflow', $yaml['name']);
+        $this->assertEquals($yaml['name'], $dataflow->name);
+
+        // Test the dataflow imported steps.
+        $steps = $dataflow->steps();
+        // Pull out the steps from the dataflow and check their values.
+        $read = array_shift($steps);
+        $debugging = array_shift($steps);
+        $write = array_shift($steps);
+        // Test various yaml provided values to have confidence the import is working as expected.
+        $this->assertEquals($yaml['steps']['read']['description'], $read->description);
+        $this->assertEquals(Yaml::dump($yaml['steps']['write']['config']), $write->config);
+        $this->assertEquals(Yaml::dump($yaml['steps']['write']['config']), $write->config);
+        $this->assertEquals($yaml['steps']['debugging']['type'], $debugging->type);
+        // Check dependencies (read -> debugging -> write).
+        $deps = $debugging->dependencies();
+        $this->assertArrayHasKey($read->id, $deps);
+        $deps = $write->dependencies();
+        $this->assertArrayHasKey($debugging->id, $deps);
     }
 
     // PHPUnit backwards compatible methods which handles the fallback to previous version calls.
