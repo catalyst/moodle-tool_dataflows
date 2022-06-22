15cbc8aa1bc9026476c55cd4bc8a11acd1a19542
Kevin Pham <keevan.pham@gmail.com>
2022-06-22 11:41:04 +1000

    194 support more optional fragments and help text (#224)
    * feat: add support for multiline conditional SQL fragments
    
    This is by adding the 's' modifier (PCRE_DOTALL) as mentioned here: https://www.php.net/manual/en/reference.pcre.pattern.modifiers.php
    
    * docs: add help text to the SQL input, also providing a short explainer

--- classes/local/step/reader_sql.php
+++ classes/local/step/reader_sql.php
@@ -85,7 +85,7 @@ class reader_sql extends reader_step {
             '\${{(?<expression>.*)}}' .
             ').*' .   // End of expressionwrapper.
             ')\]\]' . // End of fragment.
-            ')/mU',   // End of fragment wrapper.
+            ')/msU',   // End of fragment wrapper.
             $rawsql,
             $matches,
             PREG_SET_ORDER);
@@ -153,7 +153,15 @@ class reader_sql extends reader_step {
      */
     public function form_add_custom_inputs(\MoodleQuickForm &$mform) {
         // SQL.
+        $sqlexamples = "
+            SELECT id, username
+              FROM {user}
+          [[ WHERE id > \${{countervalue}} ]]
+          ORDER BY id ASC
+             LIMIT 10";
+        $sqlexamples = \html_writer::tag('pre', trim($sqlexamples, " \t\r\0\x0B"));
         $mform->addElement('textarea', 'config_sql', get_string('reader_sql:sql', 'tool_dataflows'), ['cols' => 50, 'rows' => 7]);
+        $mform->addElement('static', 'config_sql_help', '', get_string('reader_sql:sql_help', 'tool_dataflows', $sqlexamples));
         // Counter field.
         $mform->addElement('text', 'config_counterfield', get_string('reader_sql:counterfield', 'tool_dataflows'));
         $mform->addElement('static', 'config_counterfield_help', '', get_string('reader_sql:counterfield_help', 'tool_dataflows'));
