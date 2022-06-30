catalyst/moodle-tool_dataflows

commit 1eddb476acfe52b3ac0b60167e781e0f87cd66de

fix: add validation for step name and alias fields (#249)
Resolves #247

classes/form/step_form.php
@@ -144,10 +144,41 @@ class step_form extends \core\form\persistent {
      * @return array of additional errors, or overridden errors.
      */
     protected function extra_validation($data, $files, array &$errors) {
+        global $DB;
+
+        $errors = [];
+
         // Process and convert the received data back under the config field.
         $steptype = new $data->type();
         $newerrors = $steptype->form_extra_validation($data, $files, $errors);
 
-        return $newerrors;
+        // Fetch name and aliases for existing steps in this dataflow.
+        $records = $DB->get_records(
+            self::$persistentclass::TABLE,
+            ['dataflowid' => $data->dataflowid],
+            '',
+            'id, name, alias'
+        ) ?? [];
+
+        // Existing step update, remove its name and alias from the blocklist.
+        $persistent = $this->get_persistent();
+        $id = $persistent->id;
+        if (!empty($id)) {
+            unset($records[$id]);
+        }
+        $names = array_column($records, 'name');
+        $aliases = array_column($records, 'alias');
+
+        // Check and ensure the name aren't reused in new or by other existing steps.
+        if (in_array($data->name, $names)) {
+            $errors['name'] = get_string('nametaken', 'tool_dataflows', $data->name);
+        }
+
+        // Check and ensure the aliases aren't reused in new or by other existing steps.
+        if (in_array($data->alias, $aliases)) {
+            $errors['alias'] = get_string('aliastaken', 'tool_dataflows', $data->name);
+        }
+
+        return array_merge($errors, $newerrors);
     }
 }

lang/en/tool_dataflows.php
@@ -128,6 +128,8 @@ $string['requires'] = 'Requires {$a->str1} and {$a->str2}.';
 $string['stepchooser'] = 'Step chooser';
 
 // Warnings / Errors.
+$string['nametaken'] = 'The name \'{$a}\' is already in use for this dataflow.';
+$string['aliastaken'] = 'The alias \'{$a}\' is already in use for this dataflow.';
 $string['recursiveexpressiondetected'] = 'The field \'{$a->field}\' contains or is part of, a self referencing expression \'{$a->steptype}\'.';
 $string['variablefieldnotexpected'] = 'The field \'{$a->field}\' cannot be set as it is not an expected field for step type \'{$a->steptype}\'.';
 $string['invalidyaml'] = 'The content provided is invalid YAML.';

commit 0797fdd86f2a528185ff445888b17d3148b85a0c

fix: error message contained the wrong alias used
Introduced in #249

classes/form/step_form.php
@@ -176,7 +176,7 @@ class step_form extends \core\form\persistent {
 
         // Check and ensure the aliases aren't reused in new or by other existing steps.
         if (in_array($data->alias, $aliases)) {
-            $errors['alias'] = get_string('aliastaken', 'tool_dataflows', $data->name);
+            $errors['alias'] = get_string('aliastaken', 'tool_dataflows', $data->alias);
         }
 
         return array_merge($errors, $newerrors);