272a7adbae4595dcf82e000f608b6df6f2301fef
Kevin Pham <keevan.pham@gmail.com>
2022-07-01 11:56:17 +1000

    fix: bump version & add db changes in upgrade.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022070500;
-$plugin->release = 2022070500;
+$plugin->version = 2022070501;
+$plugin->release = 2022070501;
 
 $plugin->requires = 2017051500;    // Our lowest supported Moodle (3.3.0).
 
