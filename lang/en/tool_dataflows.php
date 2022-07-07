4c0c6cde1ee2e4068ed25998ca091f10b8995cff
Marc-Alexandre Ghaly <marc-alexandreghaly@catalyst-ca.net>
2022-07-06 20:09:24 -0400

    Resolves flow web service step #13

--- lang/en/tool_dataflows.php
+++ lang/en/tool_dataflows.php
@@ -98,6 +98,7 @@ $string['step_name_trigger_cron'] = 'Scheduled task trigger';
 $string['step_name_writer_debugging'] = 'Debugging writer';
 $string['step_name_writer_stream'] = 'Stream writer';
 $string['step_name_connector_debug_file_display'] = 'File contents display';
+$string['step_name_flow_web_service'] = 'Flow web service';
 
 // Step (type) groups.
 $string['stepgrouptriggers'] = 'Triggers';
@@ -299,3 +300,19 @@ $string['connector_curl:timeout'] = 'Timeout';
 $string['connector_curl:timeout_help'] = 'Time after which curl request will abort, default is 60 seconds.';
 $string['connector_curl:field_headers_help'] = 'Headers should be in the following JSON format: {$a->json} You can also use the following YAML format: {$a->yaml}';
 $string['connector_curl:output_response_result'] = 'Returns a string that contains the response to the request as text, or null if the request was unsuccessful or has not yet been sent.';
+
+// Flow Web service.
+$string['flow_web_service:webservice'] = 'Web service';
+$string['flow_web_service:webservice_help'] = 'Web service name to call ie: auth_email_get_signup_settings';
+$string['flow_web_service:user'] = 'User calling Web service';
+$string['flow_web_service:selectuser'] = 'Select user';
+$string['flow_web_service:parameters'] = 'Parameters';
+$string['flow_web_service:parameters_help'] = 'Parameters passed to the webservice in JSON format';
+$string['flow_web_service:failure'] = 'Failure processing';
+$string['flow_web_service:failure_help'] = 'In case web service call fails, either record, abort step only or abort entire flow';
+$string['flow_web_service:recordfailure'] = 'Record failure';
+$string['flow_web_service:abortstep'] = 'Abort step';
+$string['flow_web_service:abortflow'] = 'Abort flow';
+$string['flow_web_service:field_parameters_help'] = 'Parameters should be in the following YAML format: {$a->yaml} You can also use the following JSON format <br> Note: Structure depends on webservice called
+<br><br> You can reference a value from JSON with the following : <code>data.customkey <br> ie : data.GUID - data.username</code>';
+$string['flow_web_service:path'] = 'Failure path recording';
