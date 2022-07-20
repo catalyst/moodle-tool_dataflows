12abd6cc61bdb88a33e21a89cf6a7a5e04939814
Brendan Heywood <brendan@catalyst-au.net>
2022-07-21 09:55:05 +1000

    Version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022071900;
-$plugin->release = 2022071900;
+$plugin->version = 2022072100;
+$plugin->release = 2022072100;
 
 $plugin->requires = 2017051500;    // Our lowest supported Moodle (3.3.0).
 
