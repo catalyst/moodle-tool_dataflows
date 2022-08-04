707c631c615156ea62c1280bfe2716f870707386
Kevin Pham <keevan.pham@gmail.com>
2022-08-04 10:13:14 +1000

    fix: step to be extended from flow_step to correct output links
    Resolves #418

--- classes/local/step/flow_web_service.php
+++ classes/local/step/flow_web_service.php
@@ -29,7 +29,7 @@ use tool_dataflows\parser;
  * @copyright  Catalyst IT, 2022
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class flow_web_service extends flow_logic_step {
+class flow_web_service extends flow_step {
 
     /** @var bool whether or not this step type (potentially) has side effects, this will vary depending on Web Service */
     protected $hassideeffect = true;
