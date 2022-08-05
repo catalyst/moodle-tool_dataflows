2d61448e756294d5dac0beb0474a3f15a45cf1cd
Peter Sistrom <petersistrom@catalyst-au.net>
2022-08-05 15:14:19 +1000

    Issue #353 Bump version, cleanup strings

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022080401;
-$plugin->release = 2022080401;
+$plugin->version = 2022080500;
+$plugin->release = 2022080500;
 
 $plugin->requires = 2017051500;    // Our lowest supported Moodle (3.3.0).
 
