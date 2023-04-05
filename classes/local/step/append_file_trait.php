aff36137f6bbf6b590fe504b3e6a129ff5a28e75
Jason den Dulk <jasondendulk@catalyst-au.net>
2023-04-05 10:02:22 +1000

    Add append step

---
+++ classes/local/step/append_file_trait.php
@@ -0,0 +1,137 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace tool_dataflows\local\step;
+
+use tool_dataflows\helper;
+
+/**
+ * Trait for appending files. Similar to copying but will not overwrite existing files. If a
+ * destination file doesn't exist, it will be created.
+ *
+ * @package   tool_dataflows
+ * @author    Jason den Dulk <jasondendulk@catalyst-au.net>
+ * @copyright 2023, Catalyst IT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+trait append_file_trait {
+    /**
+     * Returns whether or not the step configured, has a side effect.
+     *
+     * @return     bool whether or not this step has a side effect
+     * @link https://en.wikipedia.org/wiki/Side_effect_(computer_science)
+     */
+    public function has_side_effect(): bool {
+        if (isset($this->stepdef)) {
+            $config = $this->stepdef->config;
+            return !helper::path_is_relative($config->to);
+        }
+        return true;
+    }
+
+    /**
+     * Return the definition of the fields available in this form.
+     *
+     * @return array
+     */
+    public static function form_define_fields(): array {
+        return [
+            'from' => ['type' => PARAM_TEXT, 'required' => true],
+            'to'   => ['type' => PARAM_TEXT, 'required' => true],
+        ];
+    }
+
+    /**
+     * Allows each step type to determine a list of optional/required form
+     * inputs for their configuration
+     *
+     * It's recommended you prefix the additional config related fields to avoid
+     * conflicts with any existing fields.
+     *
+     * @param \MoodleQuickForm $mform
+     */
+    public function form_add_custom_inputs(\MoodleQuickForm &$mform) {
+        // From / Source path.
+        $mform->addElement('text', 'config_from', get_string('flow_copy_file:from', 'tool_dataflows'));
+        // To / Target path.
+        $mform->addElement('text', 'config_to', get_string('flow_copy_file:to', 'tool_dataflows'));
+    }
+
+    /**
+     * Executes the step and copies what is in $from, to the $to path
+     *
+     * @param  mixed|null $input
+     * @return mixed
+     */
+    public function execute($input = null) {
+        global $CFG;
+
+        $config = $this->get_variables()->get('config');
+        $from = $this->enginestep->engine->resolve_path($config->from);
+        $to = $this->enginestep->engine->resolve_path($config->to);
+
+        // Create directory if it doesn't already exist - recursively.
+        $todirectory = dirname($to);
+        if (!file_exists($todirectory)) {
+            $this->log("Creating a directory at {$todirectory}");
+            mkdir($todirectory, $CFG->directorypermissions, true);
+        }
+
+        if (is_dir($from) || !is_readable($from)) {
+            throw new \moodle_exception('trait_append_file:cannot_read_file', 'tool_dataflows', null, $from);
+        }
+
+        if (is_dir($to)) {
+            $to .= DIRECTORY_SEPARATOR . basename($from);
+        }
+
+        if ($this->is_dry_run() && $this->has_side_effect()) {
+            return $input;
+        }
+
+        $this->log("Appending $from to $to");
+
+        $handle = fopen($from, 'r');
+        if (file_put_contents($to, $handle,  FILE_APPEND) === false) {
+            throw new \moodle_exception('flow_copy_file:copy_failed', 'tool_dataflows', (object) [
+                'from' => $from,
+                'to' => $to,
+            ]);
+        }
+        return $input;
+    }
+
+    /**
+     * Perform any extra validation that is required only for runs.
+     *
+     * @return true|array Will return true or an array of errors.
+     */
+    public function validate_for_run() {
+        $config = $this->get_variables()->get('config');
+
+        $errors = [];
+
+        $paths = ['from', 'to'];
+        foreach ($paths as $key) {
+            $error = helper::path_validate($config->$key);
+            if ($error !== true) {
+                $errors["config_{$key}"] = $error;
+            }
+        }
+
+        return empty($errors) ? true : $errors;
+    }
+}
---
+++ tests/tool_dataflows_append_file_step_test.php
@@ -0,0 +1,219 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace tool_dataflows;
+
+use Symfony\Component\Yaml\Yaml;
+use tool_dataflows\local\execution\engine;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once('backup/cc/cc_lib/gral_lib/pathutils.php');
+
+/**
+ * Unit test for append file step.
+ *
+ * @package   tool_dataflows
+ * @author    Jason den Dulk <jasondendulk@catalyst-au.net>
+ * @copyright 2023, Catalyst IT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class tool_dataflows_append_file_step_test extends \advanced_testcase {
+
+    /** Test file data. */
+    const TEST_FILE_DATA = [
+        'a.txt' => 'blah! vlah',
+        'b.txt' => "abc\nxyz",
+        'c.txt' => '  ',
+    ];
+
+    /** Existing file data. */
+    const EXISTING_FILE_DATA = [
+        'a.txt' => null,
+        'b.txt' => 'abc',
+        'c.txt' => null,
+    ];
+
+    /** Out file name. */
+    const OUT_FILE_NAME = 'out.txt';
+
+    /** File list name. */
+    const FILE_LIST_NAME = 'files.csv';
+
+    /** @var string  Base directory. */
+    private $basedir;
+
+    /**
+     * Set up before each test
+     */
+    protected function setUp(): void {
+        parent::setUp();
+        $this->resetAfterTest();
+
+        $filedata = self::TEST_FILE_DATA;
+        $filenamedata = 'fn' . PHP_EOL . implode(PHP_EOL, array_keys($filedata));
+
+        // Set up list of files to be pumped through the flow loop.
+        $basedir = sys_get_temp_dir() . DIRECTORY_SEPARATOR;
+        file_put_contents($basedir . self::FILE_LIST_NAME, $filenamedata);
+
+        // Set up source data to be read from.
+        foreach (self::TEST_FILE_DATA as $filename => $filecontent) {
+            file_put_contents($basedir . $filename, $filecontent);
+        }
+
+        $this->basedir = $basedir;
+    }
+
+    /**
+     * Tests appending to a single file.
+     *
+     * @covers \tool_dataflows\local\step\flow_append_file
+     */
+    public function test_append_one_file() {
+        // Remove the destination file.
+        if (file_exists($this->basedir . self::OUT_FILE_NAME)) {
+            unlink($this->basedir . self::OUT_FILE_NAME);
+        }
+
+        // Perform the test.
+        set_config('permitted_dirs', $this->basedir, 'tool_dataflows');
+        $dataflow = $this->make_dataflow($this->basedir . self::OUT_FILE_NAME);
+
+        ob_start();
+        $engine = new engine($dataflow);
+        $engine->execute();
+        ob_get_clean();
+
+        $expecteddata = implode('', self::TEST_FILE_DATA);
+        $actualdata = file_get_contents($this->basedir . self::OUT_FILE_NAME);
+        $this->assertEquals($expecteddata, $actualdata);
+    }
+
+    /**
+     * Tests appending to many files, declared 1:1.
+     *
+     * @covers \tool_dataflows\local\step\flow_append_file
+     */
+    public function test_append_many_files() {
+        // Set up files that will be appended to.
+        foreach (self::EXISTING_FILE_DATA as $filename => $filecontent) {
+            $path = $this->basedir . '_' . $filename;
+            if (file_exists($path)) {
+                unlink($path);
+            }
+            if (!is_null($filecontent)) {
+                file_put_contents($path, $filecontent);
+            }
+        }
+
+        // Perform the test.
+        set_config('permitted_dirs', $this->basedir, 'tool_dataflows');
+        $dataflow = $this->make_dataflow($this->basedir . '_${{record.fn}}');
+
+        ob_start();
+        $engine = new engine($dataflow);
+        $engine->execute();
+        ob_get_clean();
+
+        foreach (self::TEST_FILE_DATA as $name => $content) {
+            $expecteddata = self::EXISTING_FILE_DATA[$name] . $content;
+            $actualdata = file_get_contents($this->basedir . "_$name");
+            $this->assertEquals($expecteddata, $actualdata);
+        }
+    }
+
+    /**
+     * Tests appending to a directory.
+     *
+     * @covers \tool_dataflows\local\step\flow_append_file
+     */
+    public function test_append_dir() {
+        // Set up a directory (with files) that will be appended to.
+        $dir = $this->basedir . 'somedir';
+        if (file_exists($dir)) {
+            if (is_dir($dir)) {
+                rmdirr($dir);
+            } else {
+                unlink($dir);
+            }
+        }
+        mkdir($dir);
+        $dir .= DIRECTORY_SEPARATOR;
+
+        // Set up files within the directory that will be appended to.
+        foreach (self::EXISTING_FILE_DATA as $filename => $filecontent) {
+            if (file_exists($dir . $filename)) {
+                unlink($dir . $filename);
+            }
+            if (!is_null($filecontent)) {
+                file_put_contents($dir . $filename, $filecontent);
+            }
+        }
+
+        // Perform the test.
+        set_config('permitted_dirs', $this->basedir, 'tool_dataflows');
+        $dataflow = $this->make_dataflow($this->basedir . 'somedir');
+
+        ob_start();
+        $engine = new engine($dataflow);
+        $engine->execute();
+        ob_get_clean();
+
+        foreach (self::TEST_FILE_DATA as $name => $content) {
+            $expecteddata = self::EXISTING_FILE_DATA[$name] . $content;
+            $actualdata = file_get_contents($this->basedir . 'somedir' . DIRECTORY_SEPARATOR . $name);
+            $this->assertEquals($expecteddata, $actualdata);
+        }
+    }
+
+    /**
+     * Creates a dataflow to test.
+     *
+     * @param string $to Destination file parameter.
+     * @return dataflow
+     */
+    private function make_dataflow(string $to) {
+        $namespace = '\\tool_dataflows\\local\\step\\';
+
+        $dataflow = new dataflow();
+        $dataflow->enabled = true;
+        $dataflow->name = 'dataflow';
+        $dataflow->save();
+
+        $reader = new step();
+        $reader->name = 'reader';
+        $reader->type = $namespace . 'reader_csv';
+        $reader->config = Yaml::dump([
+            'path' => $this->basedir . self::FILE_LIST_NAME,
+            'delimiter' => ',',
+            'headers' => '',
+        ]);
+        $dataflow->add_step($reader);
+
+        $step = new step();
+        $step->name = 'appender';
+        $step->type = $namespace . 'flow_append_file';
+        $step->depends_on([$reader]);
+        $step->config = Yaml::dump([
+            'from' => $this->basedir . '${{record.fn}}',
+            'to' => $to,
+        ]);
+        $dataflow->add_step($step);
+
+        return $dataflow;
+    }
+}
