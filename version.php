db888ef6746e218bcb22913ed4966905176710b6
Kevin Pham <keevan.pham@gmail.com>
2023-07-25 13:53:06 +1000

    feat: named capture group support for regex transformer step
    Resolves #816

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023072100;
-$plugin->release = 2023072100;
+$plugin->version = 2023072500;
+$plugin->release = 2023072500;
 $plugin->requires = 2017051500;    // Our lowest supported Moodle (3.3.0).
 $plugin->supported = [35, 401];    // Available as of Moodle 3.9.0 or later.
 // TODO $plugin->incompatible = ;  // Available as of Moodle 3.9.0 or later.
