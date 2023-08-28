dce86202b4abe4c3973b93f7a4889ecbf780d6c6
Matthew Hilton <matthewhilton@catalyst-au.net>
2023-08-28 10:19:13 +1000

    [#628] version bump

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023081100;
-$plugin->release = 2023081100;
+$plugin->version = 2023082800;
+$plugin->release = 2023082800;
 $plugin->requires = 2017051500;    // Our lowest supported Moodle (3.3.0).
 $plugin->supported = [35, 401];    // Available as of Moodle 3.9.0 or later.
 // TODO $plugin->incompatible = ;  // Available as of Moodle 3.9.0 or later.
