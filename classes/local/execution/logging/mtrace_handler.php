63311dc8d01f6f3c5fb3d8c6c4fdb5626a2311b6
Guillaume Barat <guillaumebarat@catalyst-au.net>
2023-10-16 15:58:07 +1000

    add description

--- classes/local/execution/logging/mtrace_handler.php
+++ classes/local/execution/logging/mtrace_handler.php
@@ -44,6 +44,7 @@ class mtrace_handler extends AbstractProcessingHandler {
     }
 
     /**
+     * Writes the record down to the log of the implementing handler
      * @param array $record
      * @return void
      */
