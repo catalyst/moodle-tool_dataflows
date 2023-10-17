718245ea76b905fa90938f26ccc1fda110b5a660
Guillaume Barat <guillaumebarat@catalyst-au.net>
2023-10-18 09:07:09 +1000

    update to the minimum compatible php version, composer update to get monolog-bridge v5.2*, update mtrace_handler class

--- vendor/symfony/monolog-bridge/Formatter/ConsoleFormatter.php
+++ vendor/symfony/monolog-bridge/Formatter/ConsoleFormatter.php
@@ -27,10 +27,10 @@ use Symfony\Component\VarDumper\Dumper\CliDumper;
  */
 class ConsoleFormatter implements FormatterInterface
 {
-    const SIMPLE_FORMAT = "%datetime% %start_tag%%level_name%%end_tag% <comment>[%channel%]</> %message%%context%%extra%\n";
-    const SIMPLE_DATE = 'H:i:s';
+    public const SIMPLE_FORMAT = "%datetime% %start_tag%%level_name%%end_tag% <comment>[%channel%]</> %message%%context%%extra%\n";
+    public const SIMPLE_DATE = 'H:i:s';
 
-    private static $levelColorMap = [
+    private const LEVEL_COLOR_MAP = [
         Logger::DEBUG => 'fg=white',
         Logger::INFO => 'fg=green',
         Logger::NOTICE => 'fg=blue',
@@ -53,32 +53,14 @@ class ConsoleFormatter implements FormatterInterface
      *   * colors: If true, the log string contains ANSI code to add color;
      *   * multiline: If false, "context" and "extra" are dumped on one line.
      */
-    public function __construct($options = [])
+    public function __construct(array $options = [])
     {
-        // BC Layer
-        if (!\is_array($options)) {
-            @trigger_error(sprintf('The constructor arguments $format, $dateFormat, $allowInlineLineBreaks, $ignoreEmptyContextAndExtra of "%s" are deprecated since Symfony 3.3 and will be removed in 4.0. Use $options instead.', self::class), \E_USER_DEPRECATED);
-            $args = \func_get_args();
-            $options = [];
-            if (isset($args[0])) {
-                $options['format'] = $args[0];
-            }
-            if (isset($args[1])) {
-                $options['date_format'] = $args[1];
-            }
-            if (isset($args[2])) {
-                $options['multiline'] = $args[2];
-            }
-            if (isset($args[3])) {
-                $options['ignore_empty_context_and_extra'] = $args[3];
-            }
-        }
-
         $this->options = array_replace([
             'format' => self::SIMPLE_FORMAT,
             'date_format' => self::SIMPLE_DATE,
             'colors' => true,
             'multiline' => false,
+            'level_name_format' => '%-9s',
             'ignore_empty_context_and_extra' => true,
         ], $options);
 
@@ -88,7 +70,7 @@ class ConsoleFormatter implements FormatterInterface
                 '*' => [$this, 'castObject'],
             ]);
 
-            $this->outputBuffer = fopen('php://memory', 'r+b');
+            $this->outputBuffer = fopen('php://memory', 'r+');
             if ($this->options['multiline']) {
                 $output = $this->outputBuffer;
             } else {
@@ -101,6 +83,8 @@ class ConsoleFormatter implements FormatterInterface
 
     /**
      * {@inheritdoc}
+     *
+     * @return mixed
      */
     public function formatBatch(array $records)
     {
@@ -113,12 +97,12 @@ class ConsoleFormatter implements FormatterInterface
 
     /**
      * {@inheritdoc}
+     *
+     * @return mixed
      */
     public function format(array $record)
     {
         $record = $this->replacePlaceHolder($record);
-
-        $levelColor = self::$levelColorMap[$record['level']];
 
         if (!$this->options['ignore_empty_context_and_extra'] || !empty($record['context'])) {
             $context = ($this->options['multiline'] ? "\n" : ' ').$this->dumpData($record['context']);
@@ -136,8 +120,8 @@ class ConsoleFormatter implements FormatterInterface
             '%datetime%' => $record['datetime'] instanceof \DateTimeInterface
                 ? $record['datetime']->format($this->options['date_format'])
                 : $record['datetime'],
-            '%start_tag%' => sprintf('<%s>', $levelColor),
-            '%level_name%' => sprintf('%-9s', $record['level_name']),
+            '%start_tag%' => sprintf('<%s>', self::LEVEL_COLOR_MAP[$record['level']]),
+            '%level_name%' => sprintf($this->options['level_name_format'], $record['level_name']),
             '%end_tag%' => '</>',
             '%channel%' => $record['channel'],
             '%message%' => $this->replacePlaceHolder($record)['message'],
@@ -151,7 +135,7 @@ class ConsoleFormatter implements FormatterInterface
     /**
      * @internal
      */
-    public function echoLine($line, $depth, $indentPad)
+    public function echoLine(string $line, int $depth, string $indentPad)
     {
         if (-1 !== $depth) {
             fwrite($this->outputBuffer, $line);
@@ -161,7 +145,7 @@ class ConsoleFormatter implements FormatterInterface
     /**
      * @internal
      */
-    public function castObject($v, array $a, Stub $s, $isNested)
+    public function castObject($v, array $a, Stub $s, bool $isNested): array
     {
         if ($this->options['multiline']) {
             return $a;
@@ -175,11 +159,11 @@ class ConsoleFormatter implements FormatterInterface
         return $a;
     }
 
-    private function replacePlaceHolder(array $record)
+    private function replacePlaceHolder(array $record): array
     {
         $message = $record['message'];
 
-        if (false === strpos($message, '{')) {
+        if (!str_contains($message, '{')) {
             return $record;
         }
 
@@ -198,7 +182,7 @@ class ConsoleFormatter implements FormatterInterface
         return $record;
     }
 
-    private function dumpData($data, $colors = null)
+    private function dumpData($data, bool $colors = null): string
     {
         if (null === $this->dumper) {
             return '';
