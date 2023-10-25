4a30fafdfbd95447235f9b9392fbdb19f9dfd30b
Guillaume Barat <guillaumebarat@catalyst-au.net>
2023-10-25 11:58:37 +1000

    update supported version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2023101600;
 $plugin->release = 2023101600;
 $plugin->requires = 2017051500;    // Our lowest supported Moodle (3.3.0).
-$plugin->supported = [35, 401];    // Available as of Moodle 3.9.0 or later.
+$plugin->supported = [401];    // Available as of Moodle 3.9.0 or later.
 // TODO $plugin->incompatible = ;  // Available as of Moodle 3.9.0 or later.
 $plugin->component = 'tool_dataflows';
 $plugin->maturity = MATURITY_ALPHA;
